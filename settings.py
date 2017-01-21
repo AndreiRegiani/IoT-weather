f379c1ebee673fa0722dcc33415de5c119e31f85
Andrei Regiani <andrei.cpp@gmail.com>
2017-01-21 11:21:55 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # Weather API (Powered by Dark Sky)
 DARKSKY_API = 'https://api.darksky.net/forecast'
-DARKSKY_KEY = '8783625eef14d1c5818d53128636d9d8'  # Add your key here!
+DARKSKY_KEY = ''  # Add your key here!
 
 # Coordinates for weather
 # Somewhere in Iceland:
